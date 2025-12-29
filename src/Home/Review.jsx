9d488cc39dc45ed47bc6d4403b2b7b2bb3f8ec56
rayhanbri <mohammadrayhanx0@gmail.com>
2025-12-29 13:17:03 +0600

    reviews problem solved

--- src/Home/Review.jsx
+++ src/Home/Review.jsx
@@ -3,45 +3,19 @@ import ReviewCard from './ReviewCard';
 
 const Review = () => {
     const [reviews, setReviews] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
 
     useEffect(() => {
-        const fetchReviews = async () => {
-            try {
-                const response = await fetch('https://event-hub-server-umber.vercel.app/review');
-                if (!response.ok) {
-                    throw new Error('Failed to fetch reviews');
-                }
-                const data = await response.json();
-                setReviews(data);
-            } catch (err) {
-                setError(err.message);
-            } finally {
-                setLoading(false);
-            }
-        };
+        fetch("https://event-hub-server-umber.vercel.app/review")
+            .then(res => res.json())    
+            .then(data => setReviews(data))  
+            .catch(err => console.error(err));
+    }, []); 
 
-        fetchReviews();
-    }, []);
 
-    // if (loading) return <div className="text-center">Loading...</div>;
-    // if (error) return <div className="text-center text-red-500">{error}</div>;
     return (
         <div className="container mx-auto p-4 mt-6">
 
             <h1 className="text-2xl font-bold mb-4">All Reviews</h1>
-            {
-                error && <div className="text-center text-red-500">{error}</div>
-            }
-            {
-                loading && <div className="text-center">Loading...</div>
-            }
-            {!loading && reviews.length === 0 && (
-                <div className="text-center text-gray-500">
-                    No reviews available
-                </div>
-            )}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {reviews.map((review) => (
                     <ReviewCard key={review._id} review={review} />
